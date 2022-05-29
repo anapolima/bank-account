a6c84f610120c18c49fcfcf54eb533ebbeba1a0c
anapolima <anaolilima88@gmail.com>
2022-05-28 22:59:34 -0300

    feat: Create endpoints

--- app/router/router.go
+++ app/router/router.go
@@ -1,15 +1,19 @@
 package router
 
 import (
-	"github.com/anapolima/bank-account/app/handler"
-    "github.com/gorilla/mux"
+	"github.com/anapolima/bank-account/app/handlers"
+	"github.com/gorilla/mux"
 )
 
 func Router() *mux.Router {
 
-    router := mux.NewRouter()
+	router := mux.NewRouter()
 
-    router.HandleFunc("/api/user/{id}", handler.HelloWorld).Methods("GET", "OPTIONS")
+	router.HandleFunc("/create-account", handlers.CreateAccountHandler).Methods("POST", "OPTIONS")
+	router.HandleFunc("/deposit", handlers.MakeDepositHandler).Methods("POST", "OPTIONS")
+	router.HandleFunc("/draft", handlers.MakeDraftHandler).Methods("POST", "OPTIONS")
+	router.HandleFunc("/transfer", handlers.MakeTransferHandler).Methods("POST", "OPTIONS")
+	router.HandleFunc("/extract", handlers.GetExtractHandler).Methods("GET", "OPTIONS")
 
-    return router
+	return router
 }
